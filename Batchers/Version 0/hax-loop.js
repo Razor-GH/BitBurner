2cbb38b799591057e461a373727a191983cd3578
Razor-GH <80373527+Razor-GH@users.noreply.github.com>
2023-06-07 21:23:42 -0700

    Create hax-loop.js
    This is a sort of worker script that continuously hack/grow/weaken servers that is pasted to.

---
+++ Batchers/Version 0/hax-loop.js
@@ -0,0 +1,20 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+    const target = "joesguns";
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.9;
+    const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+    while (true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+
+            await ns.grow(target);
+        } else {
+
+            await ns.hack(target);
+        }
+    }
+}
